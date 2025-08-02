a989eb3d7238e78066fe08615c227b7ba7ceb895
10974-spec <emmanuelnyakoe13@gmail.com>
2025-08-02 20:47:58 +0300

    auth pages design

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import { Card } from "@/components/ui/card";
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
+import React from "react";
+
+const Page = () => {
+  console.log('sign in view');
+  
+  return <SignInView/>
+};
+
+export default Page;
